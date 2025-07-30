4a739da12268a2d5c5a64c3b8b96139df5d62d2e
Akanksha Yadav <142004726+akankshaydav@users.noreply.github.com>
2025-07-30 19:48:09 +0530

    [832] [Time Beats: 100.00%] [Memory Beats: 68.14%] - LeetPush

--- Flipping-an-Image.cpp
+++ Flipping-an-Image.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        int n = image.size();
-        int m = image[0].size();
-
-        for (int i = 0; i < n; i++) {
-            int l = 0, r = m - 1;
-            while (l <= r) {
-                if (l == r) {
-                    image[i][l] ^= 1;
-                } else {
-                    swap(image[i][l], image[i][r]);
-                    image[i][l] ^= 1;
-                    image[i][r] ^= 1;
+        int n= image.size();
+        for(int i=0; i<n; i++){
+            int l=0;
+            int r= image[0].size()-1;
+            while(l<=r){
+                if(l==r){image[i][l]^=1;}
+                else{
+                swap(image[i][l],image[i][r]);
+                image[i][l]^=1;
+                image[i][r]^=1;
+               
                 }
                 l++;
                 r--;
+                
             }
         }
         return image;
+        
     }
 };
