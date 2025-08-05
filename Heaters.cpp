abc8d07c8826ead2eb1bb06b4f16a34102db96a2
Akanksha Yadav <142004726+akankshaydav@users.noreply.github.com>
2025-08-06 00:43:26 +0530

    [475] [Time Beats: 72.52%] [Memory Beats: 64.80%] - LeetPush

--- Heaters.cpp
+++ Heaters.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
-        int radius=0;
-        for (int i=0; i<houses.size(); i++){
-            int minDist= INT_MAX;
-            for(int j=0; j<heaters.size(); j++){
-                int dist= abs(houses[i]- heaters[j]);
-                minDist = min(dist, minDist);
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+
+        int i=0, j=0, radius=0;
+        while(i<houses.size()){
+            while(j+1< heaters.size() && abs(heaters[j+1]- houses[i])<=abs(heaters[j]-houses[i])){
+                j++;
+
+
             }
-            radius= max(radius, minDist);
+            int dist= abs(heaters[j]-houses[i]);
+            radius= max(radius, dist);
+            i++;
         }
         return radius;
+
         
     }
 };
